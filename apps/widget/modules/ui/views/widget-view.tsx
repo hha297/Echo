a6e264393a4b01b0ab616dfff57981ba118a873e
hoangha297 <ha.dh290702@gmail.com>
2025-08-19 10:10:53 +0300

    Widget Session

--- apps/widget/modules/ui/views/widget-view.tsx
+++ apps/widget/modules/ui/views/widget-view.tsx
@@ -2,6 +2,7 @@
 
 import { WidgetFooter } from '../components/widget-footer';
 import { WidgetHeader } from '../components/widget-header';
+import { WidgetAuthScreen } from '../screens/widget-auth-screen';
 
 interface WidgetViewProps {
         organizationId: string;
@@ -9,14 +10,8 @@ interface WidgetViewProps {
 export const WidgetView = ({ organizationId }: WidgetViewProps) => {
         return (
                 <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden bg-muted">
-                        <WidgetHeader>
-                                <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
-                                        <p className="font-semibold text-3xl">Hi there! </p>
-                                        <p className="font-semibold text-lg">How can we help you today?</p>
-                                </div>
-                        </WidgetHeader>
-                        <div className="flex flex-1">WidgetView {organizationId}</div>
-                        <WidgetFooter />
+                        <WidgetAuthScreen />
+                        {/* <WidgetFooter /> */}
                 </main>
         );
 };
--- packages/backend/convex/public/contactSession.ts
+++ packages/backend/convex/public/contactSession.ts
@@ -1,12 +1,12 @@
-import { defineSchema, defineTable } from 'convex/server';
 import { v } from 'convex/values';
+import { mutation } from '../_generated/server.js';
 
-export default defineSchema({
-        contactSession: defineTable({
+const SESSION_DURATION_MS = 24 * 60 * 60 * 1000;
+export const create = mutation({
+        args: {
                 name: v.string(),
                 email: v.string(),
                 organizationId: v.string(),
-                expiresAt: v.number(),
                 metadata: v.optional(
                         v.object({
                                 userAgent: v.optional(v.string()),
@@ -23,10 +23,16 @@ export default defineSchema({
                                 currentUrl: v.optional(v.string()),
                         }),
                 ),
-        })
-                .index('by_expires_at', ['expiresAt'])
-                .index('by_organization_id', ['organizationId']),
-        users: defineTable({
-                name: v.string(),
-        }),
+        },
+        handler: async (ctx, args) => {
+                const now = Date.now();
+                const expiresAt = now + SESSION_DURATION_MS;
+                const contactSessionId = await ctx.db.insert('contactSession', {
+                        name: args.name,
+                        email: args.email,
+                        organizationId: args.organizationId,
+                        expiresAt,
+                        metadata: args.metadata,
+                });
+        },
 });
